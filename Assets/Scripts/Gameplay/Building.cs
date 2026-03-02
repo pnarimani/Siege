18276a8089913a1d784567b5ae98e47820cd8b28
pc <pmnarimani@gmail.com>
2026-03-02 17:28:15 +0100

    Refactor building management: consolidate StorageBuilding and ProductionBuildingView into BuildingView, enhance resource handling, and update UI interactions.

--- Assets/Scripts/Gameplay/Building.cs
+++ Assets/Scripts/Gameplay/Building.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Collections.Generic;
+using Siege.Gameplay.UI;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 namespace Siege.Gameplay
 {
@@ -19,13 +22,37 @@ namespace Siege.Gameplay
         RepairYard,
         RationingPost,
         TradingPost,
+        Storage,
     }
 
-    public class Building : MonoBehaviour
+    public class Building : MonoBehaviour, IPointerClickHandler
     {
         public bool IsEnabled;
         public BuildingId Id;
         public int ActiveRecipeIndex;
+
+        readonly List<ResourceQuantity> _resources = new();
+        public IReadOnlyList<ResourceQuantity> Resources => _resources;
+
+        public void Add(ResourceType resource, int quantity)
+        {
+            var index = _resources.FindIndex(r => r.Resource == resource);
+            if (index == -1)
+            {
+                _resources.Add(new ResourceQuantity { Resource = resource, Quantity = quantity });
+            }
+            else
+            {
+                var rq = _resources[index];
+                rq.Quantity += quantity;
+                _resources[index] = rq;
+            }
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            UISystem.Open<BuildingView>(UILayer.Window).Show(this);
+        }
     }
 
     public class BuildingDefinition
---
+++ Assets/Scripts/Gameplay/UI/BuildingView.cs
@@ -0,0 +1,94 @@
+using System.Linq;
+using JetBrains.Annotations;
+using Unity.Properties;
+using UnityEngine;
+using UnityEngine.Localization;
+using UnityEngine.UIElements;
+
+namespace Siege.Gameplay.UI
+{
+    public class StorageViewModel
+    {
+        [UsedImplicitly] [CreateProperty] public int Fuel, Food, Water, Medicine, Materials;
+    }
+
+    public class BuildingView : MonoBehaviour
+    {
+        [SerializeField] LocalizedString _enabledString, _disabledString;
+
+        SiegeButton _button;
+        StorageViewModel _viewModel;
+        VisualElement _root;
+
+        const string ActivatedClass = "building-button--activated";
+        const string DeactiveClass = "building-button--disabled";
+
+        bool _isActivated = true;
+
+        void Awake()
+        {
+            _root = this.FindElement<VisualElement>("Root");
+            _button = this.FindElement<SiegeButton>("BuildingToggle");
+            _button.Clicked += OnButtonClicked;
+            _viewModel = new StorageViewModel();
+        }
+
+        public async void Show(Building building)
+        {
+            if (building.Id == BuildingId.Storage)
+                ShowStorage(building);
+            else
+                ShowProduction();
+
+            await Awaitable.NextFrameAsync();
+            _root.AddToClassList("building-panel--in");
+        }
+
+        void ShowProduction()
+        {
+            this.FindElement<VisualElement>("StorageBuilding").style.display = DisplayStyle.None;
+            this.FindElement<VisualElement>("ProductionBuilding").style.display = DisplayStyle.Flex;
+            SetToggled(true);
+        }
+
+        void ShowStorage(Building building)
+        {
+            this.FindElement<VisualElement>("ProductionBuilding").style.display = DisplayStyle.None;
+            var storageParent = this.FindElement<VisualElement>("StorageBuilding");
+            storageParent.style.display = DisplayStyle.Flex;
+            Assign(building, out _viewModel.Food, ResourceType.Food);
+            Assign(building, out _viewModel.Fuel, ResourceType.Fuel);
+            Assign(building, out _viewModel.Water, ResourceType.Water);
+            Assign(building, out _viewModel.Medicine, ResourceType.Medicine);
+            Assign(building, out _viewModel.Materials, ResourceType.Materials);
+            storageParent.dataSource = _viewModel;
+        }
+
+        void OnButtonClicked()
+        {
+            SetToggled(!_isActivated);
+        }
+
+        void SetToggled(bool isActivated)
+        {
+            _isActivated = isActivated;
+            if (_isActivated)
+            {
+                _button.AddToClassList(ActivatedClass);
+                _button.RemoveFromClassList(DeactiveClass);
+                _button.Text = _enabledString.GetLocalizedString();
+            }
+            else
+            {
+                _button.AddToClassList(DeactiveClass);
+                _button.RemoveFromClassList(ActivatedClass);
+                _button.Text = _disabledString.GetLocalizedString();
+            }
+        }
+
+        void Assign(Building building, out int field, ResourceType type)
+        {
+            field = (int)building.Resources.FirstOrDefault(x => x.Resource == type).Quantity;
+        }
+    }
+}

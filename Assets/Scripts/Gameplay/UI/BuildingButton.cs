d0c58546e56670a1b46c1a64d318edeb2a885e85
mac <pmnarimani@gmail.com>
2026-03-11 01:03:06 +0100

    Building Panelˆ

--- Assets/Scripts/Gameplay/UI/BuildingButton.cs
+++ Assets/Scripts/Gameplay/UI/BuildingButton.cs
@@ -7,8 +7,12 @@ namespace Siege.Gameplay.UI
     [UxmlElement]
     public partial class BuildingButton : VisualElement
     {
-        const string ClassName = "building-button", LabelClassName = "building-button__label", IconClassName = "building-button__icon";
-        
+        const string 
+            ClassName = "building-button",
+            LabelClassName = "building-button__label",
+            IconClassName = "building-button__icon",
+            IconBackgroundClassName = "building-button__icon-background";
+
         readonly Image _icon;
         readonly Label _label;
 
@@ -30,14 +34,17 @@ namespace Siege.Gameplay.UI
 
         public BuildingButton()
         {
+            var iconBg = new VisualElement();
             _icon = new Image();
             _label = new Label();
-            
+
             AddToClassList(ClassName);
             _icon.AddToClassList(IconClassName);
             _label.AddToClassList(LabelClassName);
+            iconBg.AddToClassList(IconBackgroundClassName);
 
-            Add(_icon);
+            iconBg.Add(_icon);
+            Add(iconBg);
             Add(_label);
         }
     }
--- Assets/Scripts/Gameplay/UI/GUIBuildingPanel.cs
+++ Assets/Scripts/Gameplay/UI/GUIBuildingPanel.cs
@@ -5,11 +5,13 @@ namespace Siege.Gameplay.UI
 {
     public class GUIBuildingPanel : UIToolkitView
     {
+        const string ScrollViewClass = "building-panel__scroll-view";
+
         void Awake()
         {
             var tabView = Root.Q<TabView>();
 
-            for (var j = 0; j < 5; j++)
+            for (var j = 0; j < 6; j++)
             {
                 var tab = new Tab
                 {
@@ -22,11 +24,12 @@ namespace Siege.Gameplay.UI
                 {
                     style = { flexGrow = 1 },
                 };
+                scrollView.AddToClassList(ScrollViewClass);
                 tab.Add(scrollView);
 
                 for (var i = 0; i < 5; i++)
                 {
-                    scrollView.Add(new BuildingButton());
+                    scrollView.Add(new BuildingButton { Label = "Building " + (i + 1) });
                 }
             }
         }
